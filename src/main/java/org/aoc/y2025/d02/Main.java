91c2566399d8e15b1e168e9db22f7d81d63c3db6
Paul Mitsch <paul.herzog@urlaubsguru.de>
2025-12-02 14:25:34 +0100

    fix

--- src/main/java/org/aoc/y2025/d02/Main.java
+++ src/main/java/org/aoc/y2025/d02/Main.java
@@ -24,7 +24,6 @@ public class Main {
                 }
                 if (checkPartTwo(toBeChecked)) {
                     allInvalidIdsPartTwo.add(toBeChecked);
-                    allInvalidIdsPartTwo.add(toBeChecked);
                 }
             };
         }
